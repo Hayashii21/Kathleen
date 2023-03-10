47b30bac03a847bf9f366e0149f9cc6dd914d37e
Hayashii21 <127538226+Hayashii21@users.noreply.github.com>
2023-03-11 01:09:12 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+alert( 'Happy Birthday')
+let beat = new Audio('audio.mp3')
+// Play the beat
+beat.play();
+
+// Pause/stop the beat
+beat.pause();
+
+// Reload the beat (back to the start)
+beat.load();
